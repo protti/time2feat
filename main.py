683231f7f3581fbb5ab5f8d26209caab99a67e82
protti <donatotiano@gmail.com>
2024-06-21 15:28:41 +0200

    Expanded version for include external features

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from t2f.extraction.extractor import feature_extraction
 from t2f.utils.importance_old import feature_selection
 from t2f.model.clustering import ClusterWrapper
@@ -9,7 +10,7 @@ if __name__ == '__main__':
     arr[5:] = arr[5:] * 100
 
     labels = {}  # unsupervised mode
-    # labels = {0: 'a', 1: 'a', 5: 'b', 6: 'b'}  # semi-supervised mode
+    labels = {0: 'a', 1: 'a', 5: 'b', 6: 'b'}  # semi-supervised mode
     n_clusters = 2  # Number of clusters
 
     transform_type = 'std'  # preprocessing step
@@ -18,12 +19,18 @@ if __name__ == '__main__':
     # Feature extraction
     df_feats = feature_extraction(arr, batch_size=100, p=1)
 
+    external_feat = df_feats.iloc[:, :2]
+    external_feat.columns = ["Lat", "Lon"]
+
     # Feature selection
-    context = {'model_type': model_type, 'transform_type': transform_type}
-    top_feats = feature_selection(df_feats, labels=labels, context=context)
+    context = {'model_type': model_type, 'transform_type': transform_type,'score_mode':'simple','strategy':'sk_base',
+               'k_best':False, 'pre_transform':False, 'top_k':[1], 'pfa_value':0.9}
+    top_feats = feature_selection(df_feats, labels=labels, context=context, external_feat=external_feat)
+
     df_feats = df_feats[top_feats]
+    df_feats = pd.concat([df_feats, external_feat], axis=1)
 
     # Clustering
     model = ClusterWrapper(n_clusters=n_clusters, model_type=model_type, transform_type=transform_type)
     y_pred = model.fit_predict(df_feats)
-    print(y_pred.shape)
+    print(y_pred)
--- t2f/utils/importance_old.py
+++ t2f/utils/importance_old.py
@@ -1,5 +1,6 @@
 import time
 import numpy as np
+import pandas
 import pandas as pd
 from sklearn.feature_selection import SelectKBest, VarianceThreshold
 from tsfresh.feature_selection import relevance
@@ -13,58 +14,64 @@ from t2f.model.preprocessing import apply_transformation
 from t2f.model.clustering import ClusterWrapper, cluster_metrics
 
 
-def feature_selection(df_feats: pd.DataFrame, labels: dict = None, context: dict = None):
+def feature_selection(df_feats: pd.DataFrame, labels: dict = None, context: dict = None, external_feat :pd.DataFrame = None):
+    df_feats = pd.concat([df_feats, external_feat], axis=1)
     if labels:
         train_idx = list(labels.keys())
         test_idx = [i for i in range(len(df_feats)) if i not in train_idx]
         y_train = list(labels.values())
-
         df_train_features = df_feats.iloc[train_idx, :].reset_index(drop=True)
         df_test_features = df_feats.iloc[test_idx, :]
 
         df_test_features = pd.concat([df_train_features, df_test_features], axis=0, ignore_index=True)
-        params = {
-            'transform_type': context['transform_type'],
-            'model_type': context['model_type'],
-            'score_mode': 'simple',
-            'strategy': 'sk_base',
-        }
-        new_params = simple_grid_search(df_train_features, y_train, df_test_features, params)
+        params = context
+        new_params = simple_grid_search(df_train_features, y_train, df_test_features, params, external_feat=external_feat)
         top_k = new_params['top_k']
         score_mode = new_params['score_mode']
 
         top_features = features_scoring_selection(df_train_features, y_train, mode=score_mode, top_k=top_k,
-                                                  strategy='sk_base')
+                                                  strategy=params['strategy'], pfa_values=params['pfa_value'],
+                                                  external_feat=external_feat)
     else:
         top_features = features_scoring_selection(df_feats, [], mode='simple', top_k=1,
-                                                  strategy='none')
+                                                  strategy='none', pfa_values=context['pfa_value'],external_feat=external_feat)
     return top_features
 
 
-def features_simple_selection(df: pd.DataFrame, labels: list, top_k: int = 20):
+def features_simple_selection(df: pd.DataFrame, labels: list, top_k: int = 20, external_feat:pd.DataFrame=None):
     df = df.dropna(axis='columns')
+    if external_feat is not None:
+        df.drop(columns=external_feat.columns.tolist(), inplace=True)
     sk = SelectKBest(k=min(len(df.columns), top_k))
     sk.fit(df, labels)
     return list(sk.get_feature_names_out())
 
 
 def features_scoring_selection(df: pd.DataFrame, labels: list, mode: str = 'simple', top_k: int = 20,
-                               strategy: str = 'multi'):
+                               strategy: str = 'multi',pfa_values: float=0.9, external_feat: pandas.DataFrame=None):
     """ Scoring the importance for each feature for the given labels """
     df = df.dropna(axis='columns')
     labels = pd.Series((i for i in labels))
     top_k = min(len(df.columns), top_k)
     warnings.filterwarnings("ignore", category=UserWarning)
     warnings.filterwarnings("ignore", category=RuntimeWarning)
+    if strategy == 'tsfresh' and labels.nunique() > 2:
+        strategy = 'multi'
+    elif strategy == 'multi' and labels.nunique() < 3:
+        strategy = 'tsfresh'
 
     if strategy == 'tsfresh':
         relevance_features = relevance.calculate_relevance_table(df, labels, ml_task='classification', multiclass=False)
+        if external_feat is not None:
+            relevance_features = relevance_features[~relevance_features['feature'].isin(external_feat.columns.tolist())]
         relevance_features = relevance_features.sort_values(by='p_value')
         relevance_features = relevance_features.dropna(subset=['p_value'])
         ex = pd.Series(relevance_features['p_value'], index=relevance_features['feature'])
 
     elif strategy == 'multi':
         relevance_features = relevance.calculate_relevance_table(df, labels, ml_task='classification', multiclass=True)
+        if external_feat is not None:
+            relevance_features = relevance_features[~relevance_features['feature'].isin(external_feat.columns.tolist())]
         p_columns = [col for col in relevance_features.columns if col.startswith('p_value')]
         relevance_features = relevance_features.dropna(subset=p_columns)
         relevance_features['p_value'] = relevance_features[p_columns].mean(axis=1)
@@ -75,22 +82,24 @@ def features_scoring_selection(df: pd.DataFrame, labels: list, mode: str = 'simp
         # ToDo: manage all null case
         cond = ((df == float('inf')) | (df == float('-inf'))).any(axis=0)
         df = df.drop(df.columns[cond], axis=1)
-
         sk = SelectKBest(k='all')
         sk.fit(df, labels)
         df_sk = pd.DataFrame({'score': sk.scores_, 'feature': df.columns})
         df_sk.dropna(inplace=True)
+        if df_sk is not None:
+            df_sk = df_sk[~df_sk['feature'].isin(external_feat.columns.tolist())]
         df_sk.sort_values('score', ascending=False, inplace=True)
         ex = pd.Series(df_sk['score'].values[:top_k], index=df_sk['feature'].values[:top_k])
 
     elif strategy == 'sk_pvalue':
         cond = ((df == float('inf')) | (df == float('-inf'))).any(axis=0)
         df = df.drop(df.columns[cond], axis=1)
-
         sk = SelectKBest(k='all')
         sk.fit(df, labels)
         df_sk = pd.DataFrame({'pvalues': sk.pvalues_, 'feature': df.columns})
         df_sk.dropna(inplace=True)
+        if df_sk is not None:
+            df_sk = df_sk[~df_sk['feature'].isin(external_feat.columns.tolist())]
         df_sk.sort_values('pvalues', ascending=True, inplace=True)
         ex = pd.Series(df_sk['pvalues'].values[:top_k], index=df_sk['feature'].values[:top_k])
 
@@ -100,6 +109,8 @@ def features_scoring_selection(df: pd.DataFrame, labels: list, mode: str = 'simp
         selector = VarianceThreshold()
         selector.fit(df)
         features = selector.get_feature_names_out()
+        if features is not None:
+            features = features[~np.isin(features, external_feat.columns.tolist())]
         top_k = len(features)
         ex = pd.Series([1] * top_k, index=features)
 
@@ -113,7 +124,7 @@ def features_scoring_selection(df: pd.DataFrame, labels: list, mode: str = 'simp
             top_k_feat[x] = list(df[x])
         featOrd = pd.DataFrame(top_k_feat)
         # print(featOrd)
-        feats_choosed, weight_feats = pfa_scoring(featOrd, 0.9)
+        feats_choosed, weight_feats = pfa_scoring(featOrd, pfa_values)
         for x in feats_choosed:
             feats_value[x] = list(df[x])
     elif mode == 'domain':
@@ -128,7 +139,7 @@ def features_scoring_selection(df: pd.DataFrame, labels: list, mode: str = 'simp
         # print(feat_domain.keys())
         for key in feat_domain.keys():
             featOrd = pd.DataFrame(feat_domain[key])
-            feats_choosed[key], weight_feats = pfa_scoring(featOrd, 0.9)
+            feats_choosed[key], weight_feats = pfa_scoring(featOrd, pfa_values)
         for type_feat in feats_choosed.keys():
             for key in feats_choosed[type_feat]:
                 feats_value[key] = list(df[key])
@@ -145,7 +156,7 @@ def pfa_scoring(df: pd.DataFrame, expl_var_selection: float):
     return feat_PFA, expl_variance_ration
 
 
-def simple_grid_search(df_base: pd.DataFrame, y_base: list, df_complete: pd.DataFrame, params: dict):
+def simple_grid_search(df_base: pd.DataFrame, y_base: list, df_complete: pd.DataFrame, params: dict, external_feat: pd.DataFrame=None):
     cond = 'model_type' in params and 'transform_type' in params and 'score_mode' in params
     assert cond, 'Please provide all mandatory params (model, transform and score mode)'
 
@@ -160,7 +171,7 @@ def simple_grid_search(df_base: pd.DataFrame, y_base: list, df_complete: pd.Data
     pre_transform = params.get('pre_transform', False)
 
     grid_params = {
-        'top_k': [1],
+        'top_k': [params['top_k']],
         'score_mode': [params['score_mode']],
         'transform_type': [params['transform_type']],
     }
@@ -183,9 +194,12 @@ def simple_grid_search(df_base: pd.DataFrame, y_base: list, df_complete: pd.Data
             max_rows = len(df_base)  # 800
             top_features = features_scoring_selection(df_base.iloc[:max_rows, :], y_base[:max_rows],
                                                       mode=new_params['score_mode'], top_k=new_params['top_k'],
-                                                      strategy=strategy)
+                                                      strategy=strategy, external_feat=external_feat)
         else:
-            top_features = features_simple_selection(df_base, y_base, top_k=new_params['top_k'])
+            top_features = features_simple_selection(df_base, y_base, top_k=new_params['top_k'],external_feat=external_feat)
+
+        if external_feat is not None:
+            top_features.extend(external_feat.columns.tolist())
 
         # Extract train and test
         x_train = df_base[top_features].values
